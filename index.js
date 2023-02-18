ef8b48ca8581348757b3575231b2b1b1f99be949
icon <23368266@qq.com>
2023-02-18 10:00:56 +0800

    a2222

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const pinyin=require('pinyin')
 
 app.get('/', (req, res) => {
-    res.send('欢迎使用icontest33微信云托管！')
+    let msg='中文测试'
+    msg=pinyin.pinyin(msg)
+    res.send('欢迎使用icontest33 '+msg+' 微信云托管！')
 })
 
 const port = process.env.PORT || 80
